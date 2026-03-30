d8a9f2928b8c00346184df86a67f8437c6411475
joaquin.islas <joaquin.islas@edreamsodigeo.com>
2026-03-29 21:41:41 -0300

    setting defult valud to default timezone

--- app/Module.java
+++ app/Module.java
@@ -1,9 +1,20 @@
 import com.google.inject.AbstractModule;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import startup.FlywayMigrationRunner;
+
+import java.util.TimeZone;
 
 public class Module extends AbstractModule {
     @Override
     protected void configure() {
+        Config config = ConfigFactory.load();
+        if (config.hasPath("app.timezone")) {
+            String timezone = config.getString("app.timezone");
+            TimeZone.setDefault(TimeZone.getTimeZone(timezone));
+            System.setProperty("user.timezone", timezone);
+        }
+
         bind(FlywayMigrationRunner.class).asEagerSingleton();
     }
 }
